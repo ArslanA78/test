5df24023a62910b3e0bb4590f6cddf8740657846
Arslan Amjad <31206926+ArslanA78@users.noreply.github.com>
2022-01-30 18:18:00 +0000

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -14,45 +14,30 @@ let render_projects = (slug) => {
             image: 'assets/images/mentors.jpg',
             link: 'https://github.com/abhn/Mporter',
             title: 'Mporrrrrrrter',
-            demo: 'https://mporter.co',
-            technologies: ['Flask', 'Celery', 'Python'],
-            description: "Flask web application for easy reporting updates to one's mentor. Multi-user support, easy to deploy and use.",
             categories: ['featured']
         },
         {
             image: 'assets/images/mobile-landscape.jpg',
             link: 'https://github.com/abhn/Wall-E',
             title: 'Wall-E',
-            demo: 'http://wall-e-jekyll.github.io/',
-            technologies: ['Semantic UI', 'Jekyll'],
-            description: "A modern Jekyll theme with grid frontpage, beautiful typography, mobile responsive, made with Semantic UI.",
             categories: ['featured']
         },
         {
             image: 'assets/images/collage.jpg',
             link: 'https://github.com/abhn/Marvel',
             title: 'Marvel',
-            demo: false,
-            technologies: ['Android', 'OpenCV'],
-            description: "Attendance marking tool that uses face recognition for marking attendance and firebase for tracking and analytics.",
             categories: ['featured']
         },
         {
             image: 'assets/images/mpw.jpg',
             link: 'https://github.com/abhn/mpw',
             title: 'Master Password',
-            demo: 'https://www.nagekar.com/mpw',
-            technologies: ['Semantic UI', 'CSS3'],
-            description: "Master Password is an ingenious password solution that makes your passwords truly impossible to lose.",
             categories: ['featured']
         },
         {
             image: 'assets/images/social-share-count.jpeg',
             link: 'https://github.com/abhn/Social-Share-Counts',
             title: 'Social Share Count',
-            demo: false,
-            technologies: ['Python'],
-            description: "Ever wondered how many times a URL has been shared on popular social networks?",
             categories: ['featured']
         },
   
