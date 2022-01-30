220cd195f6b7ec14bec6f2676318aa6f0b91b348
Arslan Amjad <31206926+ArslanA78@users.noreply.github.com>
2022-01-30 18:19:35 +0000

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -11,7 +11,7 @@ let render_projects = (slug) => {
 
     let projects_obj = [
         {
-            image: 'assets/images/mentors.jpg',
+            image: 'assets/images/req.png',
             link: 'https://github.com/abhn/Mporter',
             title: 'Mporrrrrrrter',
             categories: ['featured']
